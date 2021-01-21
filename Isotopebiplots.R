d0cee1ffd0415bbb9955bf2b5b370879457ec8f7
Beka Stiling <stilir@uw.edu>
2021-01-21 15:25:02 -0800

    Two stacked plots. Not beautified.

--- Isotopebiplots.R
+++ Isotopebiplots.R
@@ -1,7 +1,9 @@
-# Two plots
+# This script takes preliminary C & N isotope data from Angle and Killarny lakes
+# and plots them
 
-library(tidyverse)
-library(ggplot2)
+library(tidyverse) # this package is for data wrangling and tidying
+library(ggplot2) # this package is for plotting
+library(patchwork) # this package is for making multipanel plots
 
 alldata.messy <-read_csv("AngKilCN.csv")
 head(alldata.messy)
@@ -11,13 +13,15 @@ head(alldata.messy)
 alldata.messy[grep("ANG", alldata.messy$Sample_ID),"Lake"] <- "Angle"
 alldata.messy[grep("KIL", alldata.messy$Sample_ID),"Lake"] <- "Killarney"
 
-#Now I'm going to make two data sets, one for each lake.
+#I'm going to make different data sets for each lake.
+##now I'm going to consolodate these values into a mean and SD
 
+
+#Angle
 AngleCN <- alldata.messy %>% 
   select(Sample_ID, Type_of_Material, d13C_VPDB, d15N_air, Lake) %>% 
   filter(Lake == "Angle")
 
-##now I'm going to consolode these values into a mean and SD
 summaryANG <-AngleCN %>% 
   group_by(Type_of_Material) %>% 
   summarize(aveC = mean(d13C_VPDB),
@@ -25,6 +29,7 @@ summaryANG <-AngleCN %>%
             sdC = sd(d13C_VPDB),
             sdN = sd(d15N_air))
 
+#Killarney
 KilCN <- alldata.messy %>% 
   select(Sample_ID, Type_of_Material, d13C_VPDB, d15N_air, Lake) %>% 
   filter(Lake == "Killarney")
@@ -37,8 +42,9 @@ summaryKIL <-KilCN %>%
             sdN = sd(d15N_air))
 
 
-#all samples only, basic plot
-ggplot(data = AngleCN, aes(x = d13C_VPDB, 
+#all individual samples, basic plots
+# In my opinion these are too busy.
+pA<-ggplot(data = AngleCN, aes(x = d13C_VPDB, 
                          y = d15N_air, 
                            color = Type_of_Material)) +
   geom_point() +
@@ -46,10 +52,34 @@ ggplot(data = AngleCN, aes(x = d13C_VPDB,
   labs(x = expression(paste(delta^{13}, "C (\u2030)")),
        y = expression(paste(delta^{15}, "N (\u2030)")))
 
-ggplot(data = KilCN, aes(x = d13C_VPDB, y = d15N_air, color = Type_of_Material)) +
+pK<-ggplot(data = KilCN, aes(x = d13C_VPDB, y = d15N_air, color = Type_of_Material)) +
   geom_point() +
   theme_minimal() +
   labs(x = expression(paste(delta^{13}, "C (\u2030)")),
        y = expression(paste(delta^{15}, "N (\u2030)")))
+
+##Summary plots
+pAsum<-ggplot(data = summaryANG, 
+       aes(x = aveC, 
+           y = aveN, 
+           color = Type_of_Material)) +
+  geom_point() +
+  theme_minimal() +
+  labs(x = expression(paste(delta^{13}, "C (\u2030)")),
+       y = expression(paste(delta^{15}, "N (\u2030)")))
+
+pKsum<-ggplot(data = summaryKIL, 
+       aes(x = aveC, 
+           y = aveN, 
+           color = Type_of_Material)) +
+  geom_point() +
+  theme_minimal() +
+  labs(x = expression(paste(delta^{13}, "C (\u2030)")),
+       y = expression(paste(delta^{15}, "N (\u2030)")))
+
+pA/pAsum
+
+pK/pKsum
+
 
 
