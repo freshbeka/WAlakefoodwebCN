1a66f1549a09f53c620eb096efb4428616d615f9
Beka Stiling <stilir@uw.edu>
2021-03-08 07:29:38 -0800

    Identfy repeated sampling events.

--- R/WaLakesCN_explore.R
+++ R/WaLakesCN_explore.R
@@ -1,6 +1,7 @@
 # This script takes preliminary C & N isotope data from several washington lakes and plots it.
 
 library(readxl) #To read the excel file 
+library(tidyverse) # to organize and plot
 
 ## read in the worksheet with the isotope data
 SIdata <- read_excel("data/WA_Lake_SI_Data2021_PRELIM.xlsx", sheet = "SI_Data")
@@ -8,5 +9,12 @@ SIdata <- read_excel("data/WA_Lake_SI_Data2021_PRELIM.xlsx", sheet = "SI_Data")
 
 head(SIdata) #check info.
 
+## first, I want to understand how many lakes have multiple sampling events.
 
+SIdata %>% group_by(Lake) %>% #group according to lake name
+  summarise(years = n_distinct(Year)) %>% # tally up the number of years
+  filter(years>1) # filter the years that greater than 0
+
+#  Nine lakes have multiple sampling events, especially pine lake.
+# I think I want to give each sampling even a  unique identifer  so I don't accidentally pool the wrong data.
 
