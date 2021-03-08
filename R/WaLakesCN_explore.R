472d1e06767af77a0a62cc225c55cb6cf75f6966
Beka Stiling <stilir@uw.edu>
2021-03-08 09:55:08 -0800

    created Lake_Year ID

--- R/WaLakesCN_explore.R
+++ R/WaLakesCN_explore.R
@@ -1,7 +1,10 @@
-# This script takes preliminary C & N isotope data from several washington lakes and plots it.
+# This script takes C & N isotope data from several Washington lowland lakes, organizes it, and plots it.
 
 library(readxl) #To read the excel file 
-library(tidyverse) # to organize and plot
+library(tidyverse) # To organize and plot
+
+# Review the sheet names in order to select the correct one.  
+excel_sheets("data/WA_Lake_SI_Data2021_PRELIM.xlsx")
 
 ## read in the worksheet with the isotope data
 SIdata <- read_excel("data/WA_Lake_SI_Data2021_PRELIM.xlsx", sheet = "SI_Data")
@@ -10,15 +13,18 @@ SIdata <- read_excel("data/WA_Lake_SI_Data2021_PRELIM.xlsx", sheet = "SI_Data")
 head(SIdata) #check info.
 
 ## first, I want to understand how many lakes have multiple sampling events.
-
 SIdata %>% group_by(Lake) %>% #group according to lake name
   summarise(years = n_distinct(Year)) %>% # tally up the number of years
   filter(years>1) # filter the years that greater than 0
 
 #  Nine lakes have multiple sampling events, especially pine lake.
 # I want to give each sampling even a  unique identifier so I don't accidentally pool the wrong data. The easiest way is to create a combo of Lake and Year (probably).
+SIdata_tidy <-SIdata %>% 
+  unite(col="Lake_Year", c(Lake, Year), sep="_", remove = FALSE) #create a new column called Lake_Year, based on the data from Lake & Year, separate with _, don't remove the previous Lake and Year Columns.
 
-# Pausing to wake up Brook for school. When I return I want to create a new field with Lake_Year which should become a unique ID for  each sampling event.
+#Now I want to view the groups and see what is in each group.
+SIdata_tidy %>% distinct(Group) # great, this is helpful.
 
-SIdata %>% 
+SIdata_tidy %>% group_by(Lake_Year,Group) %>% summarize(total= n())
+
 
