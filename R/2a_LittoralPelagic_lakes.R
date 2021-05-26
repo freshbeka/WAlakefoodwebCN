0088c13caf4ed8549da090e2cf38922ce627987f
Beka Stiling <stilir@uw.edu>
2021-05-25 19:14:43 -0700

    beginning to tidy the plots

--- R/2a_LittoralPelagic_lakes.R
+++ R/2a_LittoralPelagic_lakes.R
@@ -1,8 +1,6 @@
 ## Gastropod vs zooplankton when available.
 
-## For beka, starting on Tuesday May 25 - these mixing models need to be separated into individual and lake. Two different scripits. This is because they look the same and then I thought i edited the individual one, but it was the group one. 
-
-## Plan here, search for the lakes that have "end member data" and quickly calculate pelagic vs littoral reliance by the pumpkinseed. If this is the dirctoin we want to go, I'll need to be stratgic about getting end members for the remaining lakes that don't have zoops and snails.
+## Plan here, search for the lakes that have "end member data" and quickly calculate pelagic vs littoral reliance by the pumpkinseed. Then be strategic about getting endmembers for the remaining lakes that don't have zoops and snails.
 
 library(tidyverse) # To organize and plot
 library(ggrepel) #for labeling species
@@ -47,10 +45,41 @@ End.memberlakes
 SImeans %>% select(Lake_Year) %>% distinct() %>% filter(!(Lake_Year %in% End.memberlakes)) 
 ## I will need to estimate or use some other data for endmembers in these 6 lakes.
 
+# calculate the littoral reliance value for each fish species ####
+#create empty dataframe
+df <- tibble(
+  Lake_Year = character(),
+  Identity = character(),
+  littoral.reliance = numeric())
 
+j<-7
+i<-3
 
+for (j in 1:length(End.memberlakes)){   
+  data <- SImeans %>% filter(Lake_Year == End.memberlakes[j])
+  
+  for (i in 1:nrow(data %>% filter(Group == "Fish"))){   
+    Cfish <- data %>% filter(Group == "Fish") %>% slice(i) %>% pull(d13C_mean) ## all fish
+    lit <-data %>% filter(Group == "Gastropod") %>% pull(d13C_mean) %>% mean() ## I determine littoral, and I take the mean incase there are more than 1 value.
+    pel <-data %>% filter(Group == "Zooplankton") %>%  pull(d13C_mean) %>% mean()## I determine pelagic. I take the mean incase there are more than 1 value.
+    a <-data %>% filter(Group == "Fish") %>% slice(i) %>% pull(Lake_Year)
+    b <-data %>% filter(Group == "Fish") %>% slice(i) %>% pull(Identity)
+    c <- (Cfish - pel)/(lit - pel) ##I calculate the reliance as if I didn't have end-member data.
+    df <- df %>% add_row(Lake_Year =a,
+                         Identity = b,
+                         littoral.reliance = c)
+  }
+  
+}
 
-ggsave("figs/density_littoralreliance_lake.png",p2,  width = 10, height = 6, units = "in" )
+reliance <- df
+
+p1 <- ggplot(data = reliance, aes(x = littoral.reliance, y = Identity)) +
+  geom_boxplot() + 
+  geom_jitter() + 
+  theme_minimal()
+
+ggsave("figs/boxplot_littoralreliance_species.png",p1,  width = 10, height = 6, units = "in" )
 
 
 
--- R/2b_LittoralPelagic_individual.R
+++ R/2b_LittoralPelagic_individual.R
@@ -1,6 +1,11 @@
 
-# calculate the littoral reliance value for each fish species ####
-#create empty dataframe
+## Run 2a, then immediately run 2b.
+
+# Confirm subset is still available
+SIdata_subset
+
+### Now I do an individual consumer mixing model. ####
+
 df <- tibble(
   Lake_Year = character(),
   Identity = character(),
@@ -10,12 +15,12 @@ j<-7
 i<-3
 
 for (j in 1:length(End.memberlakes)){   
-  data <- SImeans %>% filter(Lake_Year == End.memberlakes[j])
+  data <- SIdata_subset %>% filter(Lake_Year == End.memberlakes[j])
   
   for (i in 1:nrow(data %>% filter(Group == "Fish"))){   
-    Cfish <- data %>% filter(Group == "Fish") %>% slice(i) %>% pull(d13C_mean) ## all fish
-    lit <-data %>% filter(Group == "Gastropod") %>% pull(d13C_mean) %>% mean() ## I determine littoral, and I take the mean incase there are more than 1 value.
-    pel <-data %>% filter(Group == "Zooplankton") %>%  pull(d13C_mean) %>% mean()## I determine pelagic. I take the mean incase there are more than 1 value.
+    Cfish <- data %>% filter(Group == "Fish") %>% slice(i) %>% pull(d13C) ## all fish
+    lit <-data %>% filter(Group == "Gastropod") %>% pull(d13C) %>% mean() ## I determine littoral, and I take the mean incase there are more than 1 value.
+    pel <-data %>% filter(Group == "Zooplankton") %>%  pull(d13C) %>% mean()## I determine pelagic. I take the mean incase there are more than 1 value.
     a <-data %>% filter(Group == "Fish") %>% slice(i) %>% pull(Lake_Year)
     b <-data %>% filter(Group == "Fish") %>% slice(i) %>% pull(Identity)
     c <- (Cfish - pel)/(lit - pel) ##I calculate the reliance as if I didn't have end-member data.
@@ -26,54 +31,42 @@ for (j in 1:length(End.memberlakes)){
   
 }
 
-reliance <- df
-
-p1 <- ggplot(data = reliance, aes(x = littoral.reliance, y = Identity)) +
-  geom_boxplot() + 
-  geom_jitter() + 
-  theme_minimal()
-
-ggsave("figs/boxplot_littoralreliance_species.png",p1,  width = 10, height = 6, units = "in" )
-
-### Now I do an individual consumer mixing model. ####
-SIdata_tidy
-
-df <- tibble(
-  Lake_Year = character(),
-  Identity = character(),
-  littoral.reliance = numeric(),
-  d13C = numeric(),
-  richness = numeric(),
-  group = character())
-
-j<-7
-i<-3
-
-for (j in 1:length(End.memberlakes)){   
-  data <- SIdata_tidy %>% filter(Lake_Year == End.memberlakes[j])
-  
-  for (i in 1:nrow(data %>% filter(Group == "Fish"))){   
-    Cfish <- data %>% filter(Group == "Fish") %>% slice(i) %>% pull(d13C) ## all fish
-    lit <-data %>% filter(Group == "Gastropod") %>% pull(d13C) %>% mean() ## I determine littoral, and I take the mean incase there are more than 1 value.
-    pel <-data %>% filter(Group == "Zooplankton") %>%  pull(d13C) %>% mean()## I determine pelagic. I take the mean incase there are more than 1 value.
-    a <-data %>% filter(Group == "Fish") %>% slice(i) %>% pull(Lake_Year)
-    b <-data %>% filter(Group == "Fish") %>% slice(i) %>% pull(Identity)
-    c <- (Cfish - pel)/(lit - pel) ##I calculate the reliance as if I didn't have end-member data.
-    d <- data %>% filter(Group == "Fish") %>% nrow()
-    df <- df %>% add_row(Lake_Year =a,
-                         Identity = b,
-                         littoral.reliance = c,
-                         d13C = Cfish,
-                         richness = d)
-  }
-  
-}
-
 individual.reliance <- df
 
-L.gibbosus <-individual.reliance %>% filter(Identity == "Lepomis gibbosus")
-L.macrochirus <-individual.reliance %>% filter(Identity == "Lepomis macrochirus")
+L.gibbosus <- individual.reliance %>% 
+  filter(Identity == "Lepomis gibbosus") 
+L.gibbosus_Angle <- individual.reliance %>% 
+  filter(Identity == "Lepomis gibbosus") %>% filter(Lake_Year == "Angle_2019")
+L.macrochirus <-individual.reliance %>% 
+  filter(Identity == "Lepomis macrochirus")
 
-p2 <- ggplot(data = L.gibbosus, aes(x = littoral.reliance, y = Identity)) +
-  geom_boxplot()  + 
-  theme_minimal()
+Angle.Killarney.individuals <- bind_rows(L.gibbosus,L.macrochirus)
+
+A19 <- "#fdae61"
+K19 <- "#2c7bb6"
+oth <- "#abd9e9"
+
+
+p2 <- ggplot(data = Angle.Killarney.individuals, 
+           aes(x = littoral.reliance, 
+               color = Lake_Year)) +
+  geom_density()  + 
+  theme_minimal() +
+  scale_color_manual(values=c(A19, #pumpkinseed/angle_2019
+                              rep(oth,3), 
+                              K19,  #bluegill/killarney
+                              rep(oth,5))) +
+  geom_density(data = L.gibbosus_Angle, 
+               size = 1.5,
+               aes(x = littoral.reliance))+
+  geom_density(data = L.macrochirus, 
+               size = 1.5,
+               aes(x = littoral.reliance)) +
+  labs(colour="Lake", x = "Littoral reliance") +
+  theme(legend.position="none")
+  
+
+p2
+
+
+ggsave("figs/density_littoralreliance_indlake.png",p2,  width = 10, height = 6, units = "in" )
