894e865bf81d5994123008f392e648a57099c895
Beka Stiling <stilir@uw.edu>
2021-07-16 17:15:34 -0700

    data wrangling and recreating plots in R

--- R/3_ArsenicCN_manuscript.R
+++ R/3_ArsenicCN_manuscript.R
@@ -19,75 +19,84 @@ excel_sheets("data/Trophic2.xlsx")
 trophic_messy <- read_excel("data/Trophic2.xlsx", 
                             sheet = "results", 
                             skip = 2,
+                            col_types = c("text","text","text","text", "date", 
+                                          "numeric","numeric","numeric", "text","date",
+                                          "numeric","numeric","numeric","numeric","numeric",
+                                          "numeric", "numeric", "text", "text", "date",
+                                          "numeric", "numeric", "numeric", "numeric", "numeric",
+                                          "text"),
                             .name_repair = make_clean_names)
-
+                            
+                            
+col_types(trophic_messy)
 #Now I want to separate out the pieces of data that I know I need, and clean up the tibble
 
-#First I want to consolidate the values that have C & N
-
-isotopes_messy <- trophic_messy %>% drop_na(uc_davis_id)
-
 #some rows have the column names repeated, so remove them
-isotopes_messy <- isotopes_messy %>% filter(species != "Species")
-
-#change some columns from character to numeric
-cols.num <- c("fillet_as", "liver_as", "gill_as","d13cvpdb","d15n_air")
-isotopes_messy[cols.num] <- sapply(isotopes_messy[cols.num],as.numeric)
+trophic_messy <- trophic_messy %>% filter(species != "Species")
 
 ##Lake info
 
-isotopes_messy<-mutate(isotopes_messy, lake = if_else(lake == "Angle", "Angle Lake", lake))
+trophic_messy<-mutate(trophic_messy, lake = if_else(lake == "Angle", "Angle Lake", lake))
 
-isotopes_messy<-mutate(isotopes_messy, lake = if_else(lake == "Killarney", "Lake Killarney", lake))
+trophic_messy<-mutate(trophic_messy, lake = if_else(lake == "Killarney", "Lake Killarney", lake))
 
-##Change CMS to snail for ASLO
-isotopes_messy<-mutate(isotopes_messy, species = if_else(species == "Chinese mystery", "snail", species))
+##Change names to to scientific names and uncapitolized
+#trophic_messy<-mutate(trophic_messy, species = if_else(species == "Chinese mystery", "snail", species))
 
-isotopes_messy<-mutate(isotopes_messy, species = if_else(species == "Chironomid", "chironomid", species))
+trophic_messy<-mutate(trophic_messy, species = if_else(species == "Chironomid", "chironomid", species))
 
-isotopes_messy<-mutate(isotopes_messy, species = if_else(species == "Pumpkinseed", "pumpkinseed (Lepomis gibbosus)", species))
+trophic_messy<-mutate(trophic_messy, species = if_else(species == "Pumpkinseed", "pumpkinseed (Lepomis gibbosus)", species))
 
-isotopes_messy<-mutate(isotopes_messy, species = if_else(species == "Bluegill", "bluegill (Lepomis macrochirus)", species))
+trophic_messy<-mutate(trophic_messy, species = if_else(species == "Bluegill", "bluegill (Lepomis macrochirus)", species))
 
-isotopes_messy<-mutate(isotopes_messy, species = if_else(species == "surface periphyton", "periphyton (surface)", species))
+trophic_messy<-mutate(trophic_messy, species = if_else(species == "surface periphyton", "periphyton (surface)", species))
 
-isotopes_messy<-mutate(isotopes_messy, species = if_else(species == "macrophytes", "macrophyte", species))
+trophic_messy<-mutate(trophic_messy, species = if_else(species == "macrophytes", "macrophyte", species))
+trophic_messy$percent_inorganic<-(trophic_messy$as_v+
+                                     trophic_messy$as_iii)/(trophic_messy$as_v+
+                                                               trophic_messy$as_iii+
+                                                               trophic_messy$dma+
+                                                               trophic_messy$mma+
+                                                               trophic_messy$combined_arseno_sugars)
+#First I want to consolidate the values that have C & N and As
+isotopes_messy <- trophic_messy %>% drop_na(uc_davis_id)
+# First, remote the d15Nvalues that don't have percent inorganic with them.
+onlyAs.N <- isotopes_messy %>% filter(!is.na(percent_inorganic))
+# Second, record group data means as species, lake combos
+gd <- onlyAs.N %>% 
+  group_by(species, lake) %>% 
+  summarise(percent_inorganic = mean(percent_inorganic),
+            d15n_air  = mean(d15n_air),
+            count = n())
 
-#factor the taxa from high to low trophic level
-isotopes_messy$species <- factor(isotopes_messy$species, # Relevel species factor
-                         levels = c("pumpkinseed (Lepomis gibbosus)", "bluegill (Lepomis macrochirus)", "zooplankton", "snail", "phytoplankton", "periphyton (surface)", "chironomid", "macrophyte"))
-
-Pump <- "#e66101"
-Blue <- "#2c7bb6"
-zoo <- "#4d4d4d" #"#bababa"  #"#b35806"
-snl <- "#9970ab" #"#4d4d4d"
-phyt <- "#66bd63"
-peri <- "#b8e186"
-chron <- "#d73027"
-macro <- "#de77ae"
-
-
-ggplot(data = isotopes_messy, aes(x = d13cvpdb, y = d15n_air, color = species)) +
-  geom_point(aes(size = fillet_as, color = species), alpha = .75) +
-  scale_size(range = c(3, 17))  +
-  facet_grid(. ~ lake) +
+#Plot the data we have with the means as center points
+ggplot(data = onlyAs.N, 
+       aes(x = percent_inorganic, 
+           y = d15n_air, 
+           color = lake,
+           fill = lake,
+           shape = species)) +
+  geom_point() + 
+  geom_point(data=gd, size = 4) +
+  scale_shape_manual(values=c(8,15:18,25)) + 
   theme_bw() +
-  scale_color_manual(values=c(Pump,
-                              Blue,
-                              zoo,
-                              snl,
-                              phyt,
-                              peri,
-                              chron,
-                              macro)) +
-  labs(x = expression(italic(delta)^13*C),
+  labs(x = "% inorganic As",
        y = expression(italic(delta)^15*N), 
-       color = "Taxa",
-       size = expression(paste(
-         "Total As (",
-         mu, g, "/", g,
-         ")", sep="")))
+       color = "Lake",
+       shape = "Species")
 
-ggsave("figs/comparAs_ALSO.png", width = 7.5, height = 4.5, units = "in" )
+## Next step, go back to "trophic_messy" and figure out how to get zooplankton and phytoplaknton values organized into the tibble for plotting.
 
+## THen lable with tags similar to the littoral reliance plot.
 
+# A look at all 25 symbols
+df2 <- data.frame(x = 1:5 , y = 1:25, z = 1:25)
+p <- ggplot(df2, aes(x, y))
+p + geom_point(aes(shape = z), size = 4) +
+  scale_shape_identity()
+# While all symbols have a foreground colour, symbols 19-25 also take a
+# background colour (fill)
+p + geom_point(aes(shape = z), size = 4, colour = "Red") +
+  scale_shape_identity()
+p + geom_point(aes(shape = z), size = 4, colour = "Red", fill = "Black") +
+  scale_shape_identity()
