58268f34611b6ca87c19dbb34ae4e932d335ba05
Beka Stiling <stilir@uw.edu>
2021-08-06 13:27:32 -0700

    cleaned data

--- R/3_ArsenicCN_manuscript.R
+++ R/3_ArsenicCN_manuscript.R
@@ -5,6 +5,7 @@ library(googledrive) #to read in the data directly from googledrive
 library(readxl) #to convert the excel file into a R object
 library(janitor) # to clean up rownames
 library(tidyverse) #for organizing and plotting
+library(broom) #for tidy model output
 
 ##First, call the file from the google drive and save it in the data file location
 drive_download("https://docs.google.com/spreadsheets/d/1qe0RBzG0_HZdpPLTUukH7BOrrH4Qblte/edit#gid=890212752", 
@@ -61,41 +62,124 @@ trophic_messy$percent_inorganic<-(trophic_messy$as_v+ trophic_messy$as_iii)/(tro
 isotopes_messy <- trophic_messy %>% drop_na(uc_davis_id)
 # First, remote the d15Nvalues that don't have percent inorganic with them.
 onlyAs.N <- isotopes_messy %>% filter(!is.na(percent_inorganic))
+
+## Next step, go back to "trophic_messy" and figure out how to get zooplankton and phytoplaknton values organized into the tibble for plotting.
+
+#zooplanktotn
+zoop<-trophic_messy %>% filter(species == "zooplankton")
+
+Angle.zoo.in <- zoop %>% 
+  filter(!is.na(percent_inorganic) & lake == "Angle Lake") %>% 
+  select(lake, species, percent_inorganic, d15n_air)
+
+Angle.N15mean.zoop <- zoop %>% 
+  filter(!is.na(d15n_air) & lake == "Angle Lake") %>% 
+  select(lake, species,d15n_air) %>% 
+  summarise(ave = mean(d15n_air))
+
+Angle.zoo.in<-Angle.zoo.in %>% mutate(d15n_air = pull(Angle.N15mean.zoop))
+
+Kiln.zoo.in <- zoop %>% 
+  filter(!is.na(percent_inorganic) & lake == "Lake Killarney") %>% 
+  select(lake, species, percent_inorganic, d15n_air)
+
+Kiln.N15mean.zoop <- zoop %>% 
+  filter(!is.na(d15n_air) & lake == "Lake Killarney") %>% 
+  select(lake, species,d15n_air) %>% 
+  summarise(ave = mean(d15n_air))
+
+Kiln.zoo.in<-Kiln.zoo.in %>% mutate(d15n_air = pull(Kiln.N15mean.zoop))
+colnames(Kiln.zoo.in)
+
+#phytoplankton
+phy<-trophic_messy %>% filter(species == "phytoplankton")
+
+Angle.phy.in <- phy %>%   ##Unlike everywhere else, there are 4 here. I'll take the mean of the center two because they are the same date.
+  filter(!is.na(percent_inorganic) & lake == "Angle Lake") %>% 
+  select(lake, species, collection_date, percent_inorganic, d15n_air)     
+
+two<-Angle.phy.in %>% slice(c(1,4))
+formean<-Angle.phy.in %>% slice(c(2,3))
+meanInOrg <-formean %>% summarize(ave = mean(percent_inorganic))
+row2<-formean %>% slice(1) %>% mutate(percent_inorganic = pull(meanInOrg))
+for.data <-bind_rows(two,row2)
+Angle.phy.innew <-for.data %>% select(lake, species, percent_inorganic, d15n_air)   
+
+Angle.N15mean.phy <- phy %>% 
+  filter(!is.na(d15n_air) & lake == "Angle Lake") %>% 
+  select(lake, species,d15n_air) %>% 
+  summarise(ave = mean(d15n_air))
+
+Angle.phy.innew<-Angle.phy.innew %>% mutate(d15n_air = pull(Angle.N15mean.phy))
+
+Kiln.phy.in <- phy %>% 
+  filter(!is.na(percent_inorganic) & lake == "Lake Killarney") %>% 
+  select(lake, species, percent_inorganic, d15n_air)
+
+Kiln.N15mean.phy <- phy %>% 
+  filter(!is.na(d15n_air) & lake == "Lake Killarney") %>% 
+  select(lake, species,d15n_air) %>% 
+  summarise(ave = mean(d15n_air))
+
+Kiln.phy.in<-Kiln.phy.in %>% mutate(d15n_air = pull(Kiln.N15mean.phy))
+
+
+sampled<-onlyAs.N %>% select(lake, species, percent_inorganic, d15n_air)
+
+d15NAs_data<-bind_rows(Angle.zoo.in,Kiln.zoo.in,Angle.phy.innew,Kiln.phy.in,sampled)
+
+write_csv(d15NAs_data, "data/d15NAs_data.csv" ) #Save data for plotting starting with data if necessary
+
+
+
 # Second, record group data means as species, lake combos
-gd <- onlyAs.N %>% 
+gd <- d15NAs_data %>% 
   group_by(species, lake) %>% 
   summarise(percent_inorganic = mean(percent_inorganic),
             d15n_air  = mean(d15n_air),
             count = n())
 
 #Plot the data we have with the means as center points
-ggplot(data = onlyAs.N, 
+ggplot(data = d15NAs_data, 
        aes(x = percent_inorganic, 
            y = d15n_air, 
            color = lake,
            fill = lake,
-           shape = species)) +
+           shape = species)) + 
+  geom_smooth(method = "lm", aes(group = lake)) +
   geom_point() + 
   geom_point(data=gd, size = 4) +
-  scale_shape_manual(values=c(8,15:18,25)) + 
+  scale_shape_manual(values=c(3,7,8,15:18,25)) + 
   theme_bw() +
   labs(x = "% inorganic As",
        y = expression(italic(delta)^15*N), 
        color = "Lake",
-       shape = "Species")
+       shape = "Species") 
 
-## Next step, go back to "trophic_messy" and figure out how to get zooplankton and phytoplaknton values organized into the tibble for plotting.
+# Test H_0: percent_inorganic*lake = 0 for difference in slopes.
+mod <- lm(d15n_air~percent_inorganic*lake,data=d15NAs_data)
+summary(mod)
+tidy(mod)
 
-## Then lable with tags similar to the littoral reliance plot.
+# Test relationship with and without groups. No difference
+tidy(anova(lm(d15n_air~percent_inorganic,data=d15NAs_data),lm(d15n_air~percent_inorganic*lake,data=d15NAs_data)))
 
-# A look at all 25 symbols
-df2 <- data.frame(x = 1:5 , y = 1:25, z = 1:25)
-p <- ggplot(df2, aes(x, y))
-p + geom_point(aes(shape = z), size = 4) +
-  scale_shape_identity()
-# While all symbols have a foreground colour, symbols 19-25 also take a
-# background colour (fill)
-p + geom_point(aes(shape = z), size = 4, colour = "Red") +
-  scale_shape_identity()
-p + geom_point(aes(shape = z), size = 4, colour = "Red", fill = "Black") +
-  scale_shape_identity()
+
+
+ggplot(data = d15NAs_data, 
+       aes(x = percent_inorganic, 
+           y = d15n_air, 
+           color = lake,
+           fill = lake,
+           shape = species)) + 
+  geom_smooth(method = "lm", aes(group = lake)) +
+  geom_point() + 
+  geom_point(data=gd, size = 4) +
+  scale_shape_manual(values=c(3,7,8,15:18,25)) + 
+  theme_bw() +
+  labs(x = "% inorganic As",
+       y = expression(italic(delta)^15*N), 
+       color = "Lake",
+       shape = "Species") 
+
+
