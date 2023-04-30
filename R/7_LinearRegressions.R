498417c9d2514fdafc5f1fb1fd33a089ab2fd55e
Beka Stiling <rstiling@kingcounty.gov>
2023-04-30 12:40:22 -0700

    moved figure script and double check data

--- R/7_LinearRegressions.R
+++ R/7_LinearRegressions.R
@@ -1,6 +1,6 @@
 ## additional analysis for Erin.
 ## Using the reformatted  data emailed to beka april 8, 2022 from Erin.
-
+## Update 4/30/2023, I verified this data is still correct and the data we are using in the manuscript.
 
 ##First, read in packages that are needed
 library(readxl) #To read the excel file 
@@ -28,6 +28,8 @@ glance(sed_oxic_lm)
 
 
 ## Total arsenic in sediment vs total arsenic in community ####
+
+## periphyton
 ggplot(reformat, aes(x=littoral_sediment_totAs_mean, y = periphyton_totAs_mean, color = Lake)) +
   geom_point()
 sed_per_lm<-lm(periphyton_totAs_mean ~ littoral_sediment_totAs_mean, reformat)
@@ -96,74 +98,3 @@ ggplot(reformat, aes(x=littoral_sediment_totAs_mean, y = sunfish_iAs_mean)) +
 ised_sun_lm<-lm(sunfish_iAs_mean ~ littoral_sediment_totAs_mean, reformat)
 summary(ised_sun_lm)
 
-#all organisms
-long<-reformat %>% select(
-                    littoral_sediment_totAs_mean,
-                    periphyton_iAs_mean,
-                    phytoplankton_iAs_mean,
-                    zooplankton_iAs_mean,
-                    snail_iAs_mean,
-                    sunfish_iAs_mean) %>% 
-  pivot_longer(periphyton_iAs_mean:sunfish_iAs_mean, names_to = "species", values_to = "organism_iAs")
-
-Pump <- "#e66101"
-  Blue <- "#2c7bb6"
-    zoo <- "#4d4d4d" #"#bababa"  #"#b35806"
-      snl <- "#9970ab" #"#4d4d4d"
-        phyt <- "#66bd63"
-          peri <- "#b8e186"
-            chron <- "#d73027"
-              macro <- "#de77ae"
-
-ggplot(long, aes(x=littoral_sediment_totAs_mean, 
-                 y = organism_iAs, 
-                 color = species, 
-                 fill = species)) +
-  geom_point()+
-  geom_smooth(method = "lm", 
-              aes(group = species)) +
-  theme_bw() +
-  scale_color_manual(values=c(peri,
-                              phyt,
-                              snl,
-                              Pump,
-                              zoo))+
-  scale_fill_manual(values=c(peri,
-                             phyt,
-                             snl,
-                             Pump,
-                             zoo))+
-  labs(x = expression(paste(
-    "Total As in sediment (",
-    mu, g, "/", g,
-    ")", sep="")),
-    y = expression(paste(
-      "% inorganic As (",
-      mu, g, "/", g,
-      ")", sep="")))
-
-ggsave("figs/predict_iAs_all.png", width = 7.5, height = 4.5, units = "in" )
-
-ggplot(long %>% filter(species != "periphyton_iAs_mean"), aes(x=littoral_sediment_totAs_mean , y = organism_iAs, color = species, fill = species)) +
-  geom_point()+
-  geom_smooth(method = "lm", 
-              aes(group = species)) +
-  theme_bw() +
-  scale_color_manual(values=c(phyt,
-                              snl,
-                              Pump,
-                              zoo))+
-  scale_fill_manual(values=c(phyt,
-                             snl,
-                             Pump,
-                             zoo))+
-  labs(x = expression(paste(
-    "Total As in sediment (",
-    mu, g, "/", g,
-    ")", sep="")),
-       y = expression(paste(
-         "% inorganic As (",
-         mu, g, "/", g,
-         ")", sep="")))
-
-ggsave("figs/predict_iAs_noperi.png", width = 7.5, height = 4.5, units = "in" )
